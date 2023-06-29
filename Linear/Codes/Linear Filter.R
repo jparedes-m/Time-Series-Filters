e1eaf26b5b0197b62afecf5bbc78d146bbc39b84
Jorge Paredes <103344273+jparedes-m@users.noreply.github.com>
2023-06-28 22:37:20 -0500

    Add files via upload

---
+++ Linear/Codes/Linear Filter.R
@@ -0,0 +1,32 @@
+# Linear Filter
+
+linear <- function(data, p_estimates = FALSE, drift = TRUE){
+  yt <- select_if(data, is.numeric)[[1]]
+  date <- select_if(data, is.Date)[[1]]
+  n <- length(yt)
+  
+  t <- 1:n
+  
+  # Estimate regression ----
+  if (drift == TRUE) {
+    reg <- lm(yt ~ t)
+  }else{
+    reg <- lm(yt ~ 0+t)
+  }
+  
+  # Print coefficients ----
+  if(p_estimates == TRUE){
+    summary(reg)
+  }
+  
+  # Get the fitted values (trend) and the cycle ----
+  y_hat <-  fitted(reg); c = yt - y_hat
+  
+  # Exportation ----
+  df <- data.frame(date = date, 
+                   serie = yt,
+                   trend = y_hat,
+                   cycle = c)
+  
+  return(df)
+}
